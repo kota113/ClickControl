7b9881479c3afdb73c7d12043f3d77a4e09a1ee9
kota113 <kota113@keio.jp>
2026-06-06 18:27:33 +1000

    Initial Commit

---
+++ Clicky Browser/Clicky_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  Clicky_BrowserApp.swift
+//  Clicky Browser
+//
+//  Created by kota113 on 2026/06/06.
+//
+
+import SwiftUI
+
+@main
+struct Clicky_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Clicky Browser/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Clicky Browser
+//
+//  Created by kota113 on 2026/06/06.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
